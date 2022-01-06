30995ec0731aa617e26d877ab2de5b7bc9062ed7
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-01-06 21:18:39 +0100

    Update new-topic-dropdown.js

--- javascripts/discourse/components/new-topic-dropdown.js
+++ javascripts/discourse/components/new-topic-dropdown.js
@@ -42,11 +42,7 @@ export default DropdownSelectBoxComponent.extend({
     const composerController = getOwner(this).lookup("controller:composer");
 
     let tags = null;
-    let categoryId = this.category ? this.category.id : null;
-      
-    topicCategory = container
-      .lookup("route:topic")
-      .get("context.category.id"),
+    let categoryId = this.category ? this.category.id : this.category;
 
     if (selectedAction === "new_question") {
       categoryId = "49";
