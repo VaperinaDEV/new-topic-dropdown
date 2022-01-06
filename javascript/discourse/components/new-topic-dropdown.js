51d002c3d081308254df847cd11dd943248a42ed
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-01-06 17:47:22 +0100

    Add files via upload

---
+++ javascript/discourse/components/new-topic-dropdown.js
@@ -0,0 +1,68 @@
+import { action } from "@ember/object";
+import { getOwner } from "discourse-common/lib/get-owner";
+import Composer from "discourse/models/composer";
+import DropdownSelectBoxComponent from "select-kit/components/dropdown-select-box";
+import { computed } from "@ember/object";
+
+export default DropdownSelectBoxComponent.extend({
+  classNames: ["new-topic-dropdown"],
+
+  selectKitOptions: {
+    icons: ["plus"],
+    showFullTitle: true,
+    autoFilterable: false,
+    filterable: false,
+    showCaret: true,
+    none: "topic.create",
+  },
+
+  content: computed(function () {
+    const items = [
+      {
+        id: "new_discussion",
+        name: "Új téma",
+        description: "Kezdj egy új beszélgetést...",
+        icon: "comment",
+      },
+    ];
+
+    items.push({
+      id: "new_question",
+      name: "Kérdés",
+      description: "Hirdess gyorsan, egyszerűen...",
+      icon: "question-circle",
+    });
+
+    items.push({
+      id: "new_ad",
+      name: "Hirdetésfeladás",
+      description: "Hirdess gyorsan, egyszerűen...",
+      icon: "tag",
+    });
+
+    return items;
+  }),
+
+  @action
+  onChange(selectedAction) {
+    const composerController = getOwner(this).lookup("controller:composer");
+
+    let tags = null;
+    let categoryId = this.category ? this.category.id : null;
+
+    if (selectedAction === "new_question") {
+      categoryId = "2";
+    }
+
+    if (selectedAction === "new_ad") {
+      categoryId = "Junk";
+    }
+
+    composerController.open({
+      action: Composer.CREATE_TOPIC,
+      draftKey: Composer.DRAFT,
+      categoryId: categoryId,
+      tags: tags,
+    });
+  },
+});
