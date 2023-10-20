1aa65ba073393753fefc38a611f1bc21a119aa87
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-10-20 18:38:21 +0200

    Update new-topic-drop.js

--- javascripts/discourse/api-initializers/new-topic-drop.js
+++ javascripts/discourse/api-initializers/new-topic-drop.js
@@ -12,35 +12,27 @@ export default {
         return;
       }
 
-      api.onPageChange((url, title) => {
-        const newTopicDropHeader = document.querySelector(".select-kit.new-topic-dropdown .select-kit-header");
-
-        newTopicDropHeader.addEventListener("click", () => {
-          document.body.classList.add("new-topic-dropdown-expanded");
-        });     
-      });
-
       api.modifyClass("component:select-kit/select-kit-body", {
         pluginId: "select-kit-expanded",
         
-        @bind
-        handleClick(event) {
-        
-          if (!this.selectKit.isExpanded || !this.selectKit.mainElement()) {
-            return;
-          }
+        didInsertElement() {
+          this._super(...arguments);
+          
+          const newTopicDropHeader = document.querySelector(".select-kit.new-topic-dropdown .select-kit-header");
+
+          newTopicDropHeader.addEventListener("click", () => {
+            document.body.classList.add("new-topic-dropdown-expanded");
+          });
+        },
+
+        willDestroyElement() {
+          this._super(...arguments);
 
           const newTopicDropExpanded = document.body.classList.contains("new-topic-dropdown-expanded");
-          if (this.selectKit.isExpanded) {
+          if (newTopicDropExpanded && this.selectKit.isExpanded) {
             document.body.classList.remove("new-topic-dropdown-expanded");
-          }        
-      
-          if (this.selectKit.mainElement().contains(event.target)) {
-            return;
           }
-      
-          this.selectKit.close(event);       
-        },
+        },        
       });
     });
   },
