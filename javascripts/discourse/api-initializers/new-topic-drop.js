806b351b19d2f965f1b6ecbe160e496f620ab91e
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-10-20 18:14:11 +0200

    Update new-topic-drop.js

--- javascripts/discourse/api-initializers/new-topic-drop.js
+++ javascripts/discourse/api-initializers/new-topic-drop.js
@@ -12,26 +12,12 @@ export default {
         return;
       }
 
-      api.modifyClass("component:select-kit/select-kit-body", {
-        pluginId: "select-kit-expanded",
-        
-        @bind
-        handleClick(event) {  
-          const newTopicDropExpanded = document.body.classList.contains("new-topic-dropdown-expanded");
-          if (newTopicDropExpanded && this.selectKit.isExpanded) {
-            document.body.classList.remove("new-topic-dropdown-expanded");
-          }
-        
-          if (!this.selectKit.isExpanded || !this.selectKit.mainElement()) {
-            return;
-          }
-      
-          if (this.selectKit.mainElement().contains(event.target)) {
-            return;
-          }
-      
-          this.selectKit.close(event);
-        },
+      api.modifySelectKit("new-topic-dropdown", () => {
+        if (!this.selectKit.isExpanded) {
+          document.body.classList.add("new-topic-dropdown-expanded");
+        } else if (this.selectKit.isExpanded) {
+          document.body.classList.remove("new-topic-dropdown-expanded");
+        }
       });
     });
   },
