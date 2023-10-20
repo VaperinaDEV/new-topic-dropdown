ed41213507e4256799cd9a66442dd1dc020e7648
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-10-20 17:05:38 +0200

    Create new-topic-drop.js

---
+++ javascripts/discourse/api-initializers/new-topic-drop.js
@@ -0,0 +1,35 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { bind } from "discourse-common/utils/decorators";
+
+export default {
+  name: "new-topic-float",
+
+  initialize() {
+    withPluginApi("0.8.7", (api) => {
+      const site = api.container.lookup("site:main");
+
+      if (!site.mobileView) {
+        return;
+      }
+
+      api.modifyClass("component:select-kit/select-kit-body", {
+        pluginId: "select-kit-b",
+        @bind
+        handleClick(event) {
+          if (this.selectKit.isExpanded) {
+            document.body.classList.add("new-topic-dropdown-expanded");
+          }
+          if (!this.selectKit.isExpanded || !this.selectKit.mainElement()) {
+            return;
+          }
+      
+          if (this.selectKit.mainElement().contains(event.target)) {
+            return;
+          }
+      
+          this.selectKit.close(event);
+        },
+      });
+    });
+  },
+};
