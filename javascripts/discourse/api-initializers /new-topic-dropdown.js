83078280ed55da466e53e2efefd8a7a7e99be6b6
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-10-20 08:30:12 +0200

    Create new-topic-dropdown.js

---
+++ javascripts/discourse/api-initializers /new-topic-dropdown.js
@@ -0,0 +1,26 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "new-topic-float",
+  
+  initialize() {
+    withPluginApi("0.8.7", (api) => {
+      const site = api.container.lookup("site:main");
+
+      if (!site.mobileView) {
+        return;
+      }
+      
+      api.onPageChange((url, title) => {
+        const newTopicDropdown = document.querySelector(".new-topic-dropdown .select-kit-header");
+        
+        newTopicDropdown.addEventListener("focusin", () => {
+          document.body.classList.add("new-topic-dropdown-expanded");
+        });
+        newTopicDropdown.addEventListener("focusout", () => {
+          document.body.classList.remove("new-topic-dropdown-expanded");
+        });
+      });
+    });
+  },
+};
