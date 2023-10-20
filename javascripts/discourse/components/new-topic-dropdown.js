96cd5deeab88ac33306b9836f8ee765412c620dc
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-10-20 13:09:08 +0200

    Update new-topic-dropdown.js

--- javascripts/discourse/components/new-topic-dropdown.js
+++ javascripts/discourse/components/new-topic-dropdown.js
@@ -16,6 +16,7 @@ export default DropdownSelectBoxComponent.extend({
   },
 
   content: computed(function () {
+    document.body.classList.add("new-topic-dropdown-expanded");
     
     const hideForNewUser = this.currentUser && this.currentUser.trust_level > 0;
     
