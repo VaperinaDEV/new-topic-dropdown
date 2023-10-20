3866e525d61915c945894c8d1f7836fc380d3124
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-10-20 18:21:18 +0200

    Update new-topic-drop.js

--- javascripts/discourse/api-initializers/new-topic-drop.js
+++ javascripts/discourse/api-initializers/new-topic-drop.js
@@ -12,12 +12,35 @@ export default {
         return;
       }
 
-      api.modifySelectKit("new-topic-dropdown", () => {
-        if (!this.selectKit.isExpanded) {
+      api.onPageChange((url, title) => {
+        const newTopicDropHeader = document.querySelector(".select-kit.new-topic-dropdown .select-kit-header");
+
+        newTopicDropHeader.addEventListener("click", () => {
           document.body.classList.add("new-topic-dropdown-expanded");
-        } else if (this.selectKit.isExpanded) {
-          document.body.classList.remove("new-topic-dropdown-expanded");
-        }
+        });
+
+          const newTopicDropExpanded = document.body.classList.contains("new-topic-dropdown-expanded");
+          if (newTopicDropExpanded && this.selectKit.isExpanded) {
+            document.body.classList.remove("new-topic-dropdown-expanded");
+          }        
+      });
+
+      api.modifyClass("component:select-kit/select-kit-body", {
+        pluginId: "select-kit-expanded",
+        
+        @bind
+        handleClick(event) {
+        
+          if (!this.selectKit.isExpanded || !this.selectKit.mainElement()) {
+            return;
+          }
+      
+          if (this.selectKit.mainElement().contains(event.target)) {
+            return;
+          }
+      
+          this.selectKit.close(event);
+        },
       });
     });
   },
